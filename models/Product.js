82e42adcf237f96f8329830a1416be8536f39784
Josh Sandquist <joshdsand@gmail.com>
2023-04-26 09:25:49 -0700

    added product and tag models

--- models/Product.js
+++ models/Product.js
@@ -3,13 +3,48 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // Created all required columns for Product model
+    id: {
+      // Id is and integer, the primary key, and will auto increment.
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      // Product name is a string
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      // Price is a decimal and used the validate property to make sure it is correct
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      // Set a default value of 10 for stock and also validates it to be only a number
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      // Category_id is referenced from the category model id to create a relationship
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -5,8 +5,16 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  { //Created tag colums with an id primary key and a tag_name that will be a string
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
