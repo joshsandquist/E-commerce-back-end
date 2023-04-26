030b813ee00fb7a580cf07832191d8e248499dcb
Josh Sandquist <joshdsand@gmail.com>
2023-04-26 09:15:20 -0700

    added model for category

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Created columns for Id and name. Set id to autoIncrement and as the primary key
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
